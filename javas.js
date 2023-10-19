139937523df03433921294ab8f412a1fc96d881e
octa815 <144718777+octa815@users.noreply.github.com>
2023-10-19 23:52:45 +0200

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,55 @@
+function scroll(){
+  var myIndex = 0;
+  carousel();
+  
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName("imagenes");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 5000);    
+  }
+}
+
+function login(){
+  var modal = document.getElementById('login');
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+function registro(){
+  var modal2 = document.getElementById('registro');
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal2.style.display = "none";
+    }
+  }
+}
+
+
+function openNav() {
+  document.getElementById("panel").style.width = "250px";
+}
+  
+function closeNav() {
+  document.getElementById("panel").style.width = "0";
+}
+
+function oscuro() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+  
+
+
